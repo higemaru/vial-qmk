34d333a40752e2298f834790438fca74ddbea96f
KAWABATA, Kazumichi <higemaru@gmail.com>
2024-07-28 15:36:16 +0900

    add kewbie38

---
+++ keyboards/kewbie/kewbie38/keymaps/vial/config.h
@@ -0,0 +1,20 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x53, 0xAC, 0x98, 0x1C, 0xCC, 0x52, 0xFC, 0x19}
+
+/* CONSIDER ADDING AN UNLOCK COMBO. SEE DOCUMENTATION. */
+#define VIAL_INSECURE
+/* #define VIAL_UNLOCK_COMBO_ROWS { 0, 0 } */
+/* #define VIAL_UNLOCK_COMBO_COLS { 0, 11 } */
+
+#define WS2812_DI_PIN GP16
+/* #ifdef WS2812_DI_PIN */
+/* #	define RGBLED_NUM 1 */
+/* #	define RGBLIGHT_LIMIT_VAL 120 */
+/* #	define RGBLIGHT_DISABLE_KEYCODES */
+/* #	define RGBLIGHT_EFFECT_RAINBOW_MOOD */
+/* #	define RGBLIGHT_SLEEP */
+/* #	define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_RAINBOW_MOOD */
+/* #endif */
