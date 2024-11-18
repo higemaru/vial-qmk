5c2deda0c948247e7493c70f6a67267838f9ced4
KAWABATA, Kazumichi <higemaru@gmail.com>
2024-11-18 21:01:45 +0900

    change keymap

--- keyboards/kewbie/kewbie38/keymaps/vial/keymap.c
+++ keyboards/kewbie/kewbie38/keymaps/vial/keymap.c
@@ -8,21 +8,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             LGUI_T(KC_Q), KC_W, KC_E, KC_R, KC_T,                  KC_Y, KC_U, KC_I,    KC_O,   KC_P,
             CTL_T(KC_A), KC_S, KC_D, KC_F, KC_G,                  KC_H, KC_J, KC_K,    KC_L,  CTL_T(KC_SCLN),
             SFT_T(KC_Z), KC_X, KC_C, KC_V, KC_B,                  KC_N, KC_M, KC_COMM, KC_DOT, SFT_T(KC_MINS),
-            CTL_T(KC_TAB), LALT_T(KC_TAB), LGUI_T(KC_ESC), LT(1,KC_SPC), LT(2,KC_SPC), KC_ENT, KC_BSPC, SFT_T(KC_ESC)
+            CTL_T(KC_TAB), LALT_T(KC_TAB), LGUI_T(KC_ESC), LT(1,KC_SPC), LT(2,KC_SPC), KC_ENT, KC_TAB, SFT_T(KC_ESC)
         ),
 
         [1] = LAYOUT(
             KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
             KC_1,    KC_2,  KC_3,    KC_4,   KC_5,      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
             KC_TRNS, KC_EQL, KC_PLUS, KC_MINS, KC_GRV,          KC_QUOT,  KC_DQUO, KC_LBRC, KC_RBRC, KC_SLSH,
-            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  MO(3), KC_TRNS, KC_TRNS, KC_TRNS
+            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  MO(3), KC_BSPC, KC_TRNS, KC_TRNS
         ),
 
         [2] = LAYOUT(
             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_PIPE,
             KC_TRNS, KC_EQL,  KC_PLUS, KC_UNDS, KC_TILD,  KC_QUOT, KC_DQUO, KC_LCBR, KC_RCBR,  KC_BSLS,
-            KC_TRNS, KC_TRNS, KC_TRNS, MO(3),  KC_TRNS, KC_TRNS, KC_TRNS, SFT_T(KC_ESC)
+            KC_TRNS, KC_TRNS, KC_TRNS, MO(3),  KC_TRNS, KC_BSPC, KC_TRNS, KC_TRNS
         ),
 
 
--- keyboards/kewbie/kewbie46/keymaps/vial/keymap.c
+++ keyboards/kewbie/kewbie46/keymaps/vial/keymap.c
@@ -6,23 +6,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
         [0] = LAYOUT(
             KC_ESC,  LGUI_T(KC_Q), KC_W, KC_E, KC_R, KC_T,                  KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_RSFT,
-            KC_TAB,  CTL_T(KC_A), KC_S, KC_D, KC_F, KC_G,                  KC_H, KC_J, KC_K,    KC_L,   CTL_T(KC_SCLN), KC_GRV,
-            KC_LCTL, SFT_T(KC_Z), KC_X, KC_C, KC_V, KC_B,                  KC_N, KC_M, KC_COMM, KC_DOT, SFT_T(KC_MINS), KC_QUOT,
-            KC_LSFT, CTL_T(KC_TAB), LALT_T(KC_TAB), LGUI_T(KC_ESC), LT(1,KC_SPC), LT(2,KC_SPC), KC_ENT,  KC_BSPC,SFT_T(KC_ESC), KC_SLSH
+/*            KC_TAB,  CTL_T(KC_A), KC_S, KC_D, KC_F, KC_G,                  KC_H, KC_J, KC_K,    KC_L,   CTL_T(KC_SCLN), KC_GRV, */
+            KC_TAB,  CTL_T(KC_A), KC_S, KC_D, KC_F, KC_G,                  KC_H, KC_J, KC_K,    KC_L,   CTL_T(KC_MINS), KC_GRV,
+/*            KC_LCTL, SFT_T(KC_Z), KC_X, KC_C, KC_V, KC_B,                  KC_N, KC_M, KC_COMM, KC_DOT, SFT_T(KC_MINS), KC_QUOT, */
+            KC_LCTL, SFT_T(KC_Z), KC_X, KC_C, KC_V, KC_B,                  KC_N, KC_M, KC_COMM, KC_DOT, SFT_T(KC_SCLN), KC_QUOT,
+/*            KC_LSFT, CTL_T(KC_TAB), LALT_T(KC_TAB), LGUI_T(KC_ESC), LT(1,KC_SPC), LT(2,KC_SPC), KC_ENT,  KC_GRV,SFT_T(KC_SLSH), KC_SLSH */
+            KC_LSFT, CTL_T(KC_TAB), LALT_T(KC_TAB), LGUI_T(KC_ESC), LT(1,KC_SPC), LT(2,KC_SPC), KC_ENT,  KC_TAB,SFT_T(KC_SLSH), KC_SLSH
         ),
 
         [1] = LAYOUT(
             KC_TRNS, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TRNS,
             KC_TRNS, KC_1,    KC_2,  KC_3,    KC_4,   KC_5,      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS,
-            KC_TRNS, KC_TRNS, KC_EQL, KC_PLUS, KC_MINS, KC_GRV,          KC_QUOT, KC_DQUO,  KC_LBRC, KC_RBRC, KC_SLSH, KC_TRNS,
-            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  MO(3), KC_TRNS, KC_TRNS, KC_TRNS, KC_PIPE
+            KC_TRNS, KC_TRNS, KC_EQL, KC_PLUS, KC_MINS, KC_GRV,          KC_QUOT, KC_PIPE,  KC_LBRC, KC_RBRC, KC_SLSH, KC_TRNS,
+/*            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  MO(3), KC_BSPC, KC_TILD, KC_PIPE, KC_PIPE */
+            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  MO(3), KC_BSPC, KC_TAB, KC_PIPE, KC_PIPE
         ),
 
         [2] = LAYOUT(
             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, RGB_TOG,
             KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_PIPE,   KC_TRNS,
-            KC_TRNS, KC_TRNS, KC_EQL, KC_PLUS,   KC_UNDS, KC_TILD,  KC_QUOT, KC_DQUO, KC_LCBR, KC_RCBR,  KC_BSLS, KC_TRNS,
-            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(3),  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PIPE
+            KC_TRNS, KC_TRNS, KC_EQL, KC_PLUS,   KC_UNDS, KC_TILD,  KC_QUOT, KC_PIPE, KC_LCBR, KC_RCBR,  KC_BSLS, KC_TRNS,
+            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(3),  KC_TRNS, KC_BSPC, KC_TRNS, KC_BSLS, KC_PIPE
         ),
 
 
